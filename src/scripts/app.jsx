cfae6161240097f42d926d9736f0a7a14badc80c
Paul Phillips <paul.phillips@thoughtworks.com>
2015-01-18 17:18:46 -0600

    Filter out the selected city from the next dropdown box (you can't travel from Dallas to Dallas)

--- src/scripts/app.jsx
+++ src/scripts/app.jsx
@@ -1,4 +1,5 @@
 
+var _ = require('lodash');
 var React = require("react");
 var Reflux = require("reflux");
 
@@ -10,30 +11,85 @@ var DefaultRoute  = Router.DefaultRoute;
 var Link          = Router.Link;
 
 var actions = Reflux.createActions([
-	"checkFlightStatus"
+	"checkFlightStatus",
+	"departureCitySelected",
+	"arrivalCitySelected"
 ]);
 
-var FlightStatusSearchForm = React.createClass({
-	mixins: [Router.Navigation],
-	getInitialState: function() {
+var FlightStatusStore = Reflux.createStore({
+	init: function() {
+		this._allAirports = ['DAL', 'AUS', 'SAT'];
+		this.listenToMany(actions);
+		this.from = 'departure';
+		this.to = 'arrival';
+		this.date = '2015-01-25';
+		this.fromAirports = this._allAirports;
+		this.toAirports = this._allAirports;
+	},
+	getFormValues: function() {
 		return {
-			from: 'departure',
-			to: 'arrival',
-			date: '2015-01-25'
-		}
+			from: this.from,
+			to: this.to,
+			date: this.date,
+			fromAirports: this.fromAirports,
+			toAirports: this.toAirports
+		};
+	},
+	onCheckFlightStatus: function() {
+		this.trigger();
+	},
+	onDepartureCitySelected: function(city) {
+		this.from = city;
+		this.toAirports = _.difference(this._allAirports, [city]);
+		this.trigger();
+	},
+	onArrivalCitySelected: function(city) {
+		this.to = city;
+		this.fromAirports = _.difference(this._allAirports, [city]);
+		this.trigger();
+	}
+});
+
+var FlightStatusSearchForm = React.createClass({
+	mixins: [Router.Navigation, Reflux.listenTo(FlightStatusStore, "onStateChange")],
+	getInitialState: function() {
+		return FlightStatusStore.getFormValues();
+	},
+	onStateChange: function(state) {
+		this.setState(FlightStatusStore.getFormValues());
 	},
 	render: function() {
 		return (
 			<div>
-			  <div>
-			    <input placeholder={this.state.from}/>
-			  </div>
-			  <div>
-			    <input placeholder={this.state.to}/>
-			  </div>
+			 <AirportSelection
+			 	selection={this.state.from}
+			 	airports={this.state.fromAirports}
+			 	selectionAction={actions.departureCitySelected} />
+			 <AirportSelection 
+			 	selection={this.state.to} 
+			 	airports={this.state.toAirports}
+			 	selectionAction={actions.arrivalCitySelected} />
 			  <Link to='flightStatusSearch' params={this.state}>Search</Link>
 			</div>
 		);
+	}
+});
+
+var AirportSelection = React.createClass({
+	render: function() {
+		return (
+			<select defaultValue={this.props.selection} onChange={this._onSelect}>
+				{ 
+					this.props.airports.map(function(airport) {
+						return <option key={airport} value={airport}>{airport}</option>
+					})
+				}
+			</select>
+		);
+	},
+	_onSelect: function(event) {
+		var action = this.props.selectionAction;
+		action(event.target.value);
 	}
 });
 
@@ -47,20 +103,6 @@ var BadFlightStatusSearchRequest = React.createClass({
 			  <FlightStatusSearchForm />
 			</div>
 		);
-	}
-});
-
-var FlightStatusStore = Reflux.createStore({
-	init: function() {
-		this.listenTo(actions.checkFlightStatus, "checkFlightStatus");
-	},
-	getStatuses: function() {
-		return [{
-
-		}];
-	},
-	checkFlightStatus: function() {
-		this.trigger();
 	}
 });
 
